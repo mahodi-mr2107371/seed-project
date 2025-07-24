4837de3e3099e0e365c785d7a14ce1007e76e0c2
mahodi-mr2107371 <123658483+mahodi-mr2107371@users.noreply.github.com>
2025-07-24 16:48:45 +0300

    Logout button made responsive

--- components/logout.tsx
+++ components/logout.tsx
@@ -1,9 +1,12 @@
+import { LogOut } from 'lucide-react';
 import React from 'react'
 
 const Logout = () => {
     return (
-        <button className="p-2 bg-red-500 text-white rounded hover:bg-red-600">
-            Logout
+        <button className="rounded shadow-md">
+            <LogOut className="m-2 w-5 h-5  text-foreground md:hidden" />
+            <div className='p-2 hidden md:inline md:bg-red-500 md:text-white rounded md:hover:bg-red-600'>Logout</div>
+
         </button>
     )
 }
